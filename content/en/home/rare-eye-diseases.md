2cdd2109a5ee004b0544521194707bcb5e261877
Lunde CHEN <lunde.chen@yahoo.fr>
2019-10-07 17:21:09 +0800

    rare eye disease

---
+++ content/en/home/rare-eye-diseases.md
@@ -0,0 +1,78 @@
++++
+widget = "rare-eye-diseases"  # See https://sourcethemes.com/academic/docs/page-builder/
+headless = true  # This file represents a page section.
+active = true  # Activate this widget? true/false
+weight = 70  # Order that this section will appear.
+
+title = "Rare Eye Diseases"
+subtitle = ""
+
+# Rare Eye Diseases to_display_in_first_page
+
+[[disease]]
+  name = "Keratocunos"
+  url = ""
+
+[[disease]]
+  name = "Coats' Disease"
+  url = ""
+
+[[disease]]
+  name = "Usher Syndrome"
+  url = ""
+
+[[disease]]
+  name = "Retinitis Pigmentosa (RP)"
+  url = ""
+
+[[disease]]
+  name = "Stargardt Disease"
+  url = ""
+
+[[disease]]
+  name = "Retinoblastoma"
+  url = ""
+
+[[disease]]
+  name = "Idiopathic intracranial hypertension"
+  url = ""  
+
+[[disease]]
+  name = "Microphthalmia (missing or small eyes)"
+  url = ""
+
+[[disease]]
+  name = "Bietti’s crystalline dystrophy"
+  url = ""
+
+[[disease]]
+  name = "Behçet’s disease of the eye"
+  url = ""
+
+
+[[disease]]
+  name = "Choroideremia (CHM)"
+  url = ""
+
+[[disease]]
+  name = "Juvenile X-Linked Retinoschisis"
+  url = ""
+
+[[disease]]
+  name = "Gyrate Atrophy"
+  url = ""
+
+[[disease]]
+  name = "Leber Hereditary Optic Neuropathy"
+  url = ""
+
+[[disease]]
+  name = "Vitelliform Macular Dystrophy (Best Disease)"
+  url = ""
+
+[[disease]]
+  name = "Eye Cancer"
+  url = ""
+  
++++
+Here are a list of Rare Eye Diseases. Click to learn more about those diseases.
--- static/js/particles.slider.destroy.js
+++ static/js/particles.slider.destroy.js
@@ -1,105 +1,128 @@
-console.info("particles.slder.destroy.js;");
 
 var destroy = function (){
 	window.pJSDom[0].pJS.fn.vendors.destroypJS();
-	console.info("window.pJSDom[0].pJS.fn.vendors.destroypJS();");
+	console.log("window.pJSDom[0].pJS.fn.vendors.destroypJS();");
 }
 
 var reload = function(){
+
+	console.log("reload;");
+
+
+	// reference: https://github.com/VincentGarreau/particles.js/issues/30
+    var num_nb = 10;
+	var width = $(document).width();
+	if (width > 1300) {
+		num_nb = Math.round(Math.sqrt(width * 0.3));
+	} else {
+		num_nb = Math.min(8, Math.round(Math.sqrt(width * 0.04)));
+	}
+
+
 	window.pJSDom = [];
 	window.particlesJS(
-	   "particles-js-slider", {
-			"particles": {
-				"number": {
-					"value": 40,
-					"density": {
-						"enable": true,
-						"value_area": 800
-					}
-				},
-				"color": {
-					"value": "#399c9c"
-				},
-				"shape": {
-					"type": "circle",
-					"stroke": {
-						"width": 0,
-						"color": "#000000"
-					},
-					"polygon": {
-						"nb_sides": 5
-					},
-					// "image": {
-					//     "src": "img/github.svg",
-					//     "width": 100,
-					//     "height": 100
-					// }
-				},
-				"opacity": {
-					"value": 0.3,
-					"random": false,
-					"anim": {
-						"enable": false,
-						"speed": 1,
-						"opacity_min": 0.1,
-						"sync": false
-					}
-				},
-				"size": {
-					"value": 20,
-					"random": true,
-					"anim": {
-						"enable": false,
-						"speed": 30,
-						"size_min": 0.1,
-						"sync": false
-					}
-				},
-				"line_linked": {
+
+		"particles-js-slider", {
+		"particles": {
+			"number": {
+				"value": num_nb,
+				"density": {
 					"enable": true,
-					"distance": 250,
-					"color": "#40afaf",
-					"opacity": 0.2,
-					"width": 1
-				},
-				"move": {
-					"enable": true,
-					"speed": 3,
-					"direction": "none",
-					"random": true,
-					"straight": false,
-					"out_mode": "out",
-					"bounce": true,
-					"attract": {
-						"enable": false,
-						"rotateX": 600,
-						"rotateY": 1200
-					}
+					"value_area": 800
 				}
 			},
-			"interactivity": {
-				"detect_on": "canvas",
-				"events": {
-					"onhover": {
-						"enable": true,
-						"mode": "grab"
-					},
-					"onclick": {
-						"enable": true,
-						"mode": "push"
-					},
-					"resize": true
+			"color": {
+				"value": "#FBF8EF"
+			},
+			"shape": {
+				"type": "circle",
+				"stroke": {
+					"width": 0,
+					"color": "#000000"
 				},
-				"modes": {
-					"grab": {
-						"distance": 100,
-						"line_linked": {
-							"opacity": 0.5
-						}
-					},
+				"polygon": {
+					"nb_sides": 6
+				},
+			},
+			"opacity": {
+				"value": 0.1,
+				"random": false,
+				"anim": {
+					"enable": true,
+					"speed": 0.1,
+					"opacity_min": 0.01,
+					"sync": false
 				}
 			},
-			"retina_detect": true
-		}
-		);
+			"size": {
+				"value": 70,
+				"random": true,
+				"anim": {
+					"enable": false,
+					"speed": 30,
+					"size_min": 0.1,
+					"sync": false
+				}
+			},
+			"line_linked": {
+				"enable": true,
+				"distance": 250,
+				"color": "#E1F5A9",
+				"opacity": 0.1,
+				"width": 1
+			},
+			"move": {
+				"enable": true,
+				"speed": 2,
+				"direction": "right",
+				"random": true,
+				"straight": false,
+				"out_mode": "out",
+				"bounce": true,
+				"attract": {
+					"enable": false,
+					"rotateX": 600,
+					"rotateY": 1200
+				}
+			}
+		},
+		"interactivity": {
+			"detect_on": "",
+			"events": {
+				"onhover": {
+					"enable": false,
+					"mode": "grab"
+				},
+				"onclick": {
+					"enable": false,
+					"mode": "push"
+				},
+				"resize": false
+			},
+			"modes": {
+				"grab": {
+					"distance": 100,
+					"line_linked": {
+						"opacity": 0.3
+					}
+				},
+
+			}
+		},
+		"retina_detect": true
+	}
+
+	);
+
 }
+
+
+reload();
+
+
+window.addEventListener('resize', function () { // use ".addEventListener", not ".onresize"
+	console.log("window.addEventListener;");
+	destroy();
+	reload();
+}, true);
+
